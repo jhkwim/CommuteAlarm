0984102117cad10ab8d664e2799ec9312c7d95f4
김재훈 <jhkim@mobileleader.com>
2022-03-31 23:51:17 +0900

    테스트 코드 수정

--- app/src/test/java/com/jhkwim/commutealarm/Test.kt
+++ app/src/test/java/com/jhkwim/commutealarm/Test.kt
@@ -1,13 +1,27 @@
 package com.jhkwim.commutealarm
 
+import android.util.Log
 import com.jhkwim.commutealarm.parse.CommuteStr
 import com.jhkwim.commutealarm.parse.CommuteStringParser
 import com.jhkwim.commutealarm.utils.DateTimeUtils
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.date.*
+import io.mockk.every
+import io.mockk.mockkStatic
 import java.time.DayOfWeek
+import java.time.LocalDateTime
 
 class Test: StringSpec({
+
+    beforeEach {
+        mockkStatic(Log::class)
+        every { Log.v(any(), any()) } returns 0
+        every { Log.d(any(), any()) } returns 0
+        every { Log.i(any(), any()) } returns 0
+        every { Log.e(any(), any()) } returns 0
+        every { Log.w(any(), any<String>()) } returns 0
+
+    }
 
     "String to LocalDateTime" {
         val pattern = "yyyy년MM월dd일(E)ahh:mm"
@@ -36,7 +50,8 @@ class Test: StringSpec({
                 "퇴근 예상 시간: PM 5:00\n" +
                 "\n"
 
-        val commuteStr: CommuteStr = CommuteStringParser(text).parse()
+
+        val commuteStr: CommuteStr = CommuteStringParser(LocalDateTime.now(), text).parse()
 
         println(commuteStr)
     }
